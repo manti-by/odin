7e82517d201d5fc9b7ebc84e391064786a82acc6
Alexander Chaika <manti.by@gmail.com>
2023-12-14 11:26:22 +0300

    Update home page

--- odin/settings/dev.py
+++ odin/settings/dev.py
@@ -1,1 +1,19 @@
 from .base import *  # noqa
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "simple": {"format": "%(levelname)s %(asctime)s %(message)s"},
+    },
+    "handlers": {
+        "console": {
+            "level": "INFO",
+            "class": "logging.StreamHandler",
+            "formatter": "simple",
+        },
+    },
+    "loggers": {
+        "": {"handlers": ["console"], "level": "INFO"},
+    },
+}
