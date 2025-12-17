1583de69636460bd2d7e1694e6033f3e285a875d
Alexander Chaika <manti.by@gmail.com>
2025-12-17 11:50:07 +0300

    Update translations

--- odin/apps/sensors/apps.py
+++ odin/apps/sensors/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class SensorsConfig(AppConfig):
     name = "odin.apps.sensors"
+    verbose_name = _("sensors")
