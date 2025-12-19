51b1ec01833c0ae3884961cb8b72a23b9e885d58
Alexander Chaika <manti.by@gmail.com>
2025-12-19 09:45:08 +0300

    Update relays admin, fix uv.lock

--- odin/apps/relays/admin.py
+++ odin/apps/relays/admin.py
@@ -1,9 +1,34 @@
 from django.contrib import admin
+from django.forms import ModelForm
+from django.http import HttpRequest
+from django.template.loader import render_to_string
+from django.utils.html import format_html
+from django.utils.translation import gettext_lazy as _
 
 from .models import Relay
 
 
 @admin.register(Relay)
 class RelayAdmin(admin.ModelAdmin):
+    fields = ("relay_id", "name", "type", "is_active", "schedule_view", "updated_at", "created_at")
     list_display = ("relay_id", "name", "type", "is_active", "updated_at", "created_at")
     list_filter = ("type",)
+    readonly_fields = ("schedule_view", "updated_at", "created_at")
+
+    def schedule_view(self, obj: Relay) -> str:
+        if not (schedule := obj.context.get("schedule")):
+            return "-"
+
+        html = render_to_string("admin/schedule.html", {"schedule": schedule})
+        return format_html(html)
+
+    schedule_view.short_description = _("schedule")
+
+    def save_model(self, request: HttpRequest, obj: Relay, form: ModelForm, change: bool):
+        schedule = {str(d): {str(h): False for h in range(24)} for d in range(7)}
+        for field in form.data:
+            if field.startswith("schedule"):
+                _, day, hour = field.split("-")
+                schedule[day][hour] = True
+        obj.context.update({"schedule": schedule})
+        super().save_model(request, obj, form, change)
