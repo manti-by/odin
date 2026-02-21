3a074704c72c0e96297151b94f36c468bd88c2ee
Alexander Chaika <manti.by@gmail.com>
2026-02-21 10:58:56 +0300

    MNT-5: Exchange rates trends

--- odin/apps/currency/templatetags/currency_tags.py
+++ odin/apps/currency/templatetags/currency_tags.py
@@ -10,3 +10,8 @@ def currency_rate(value):
     if value is None:
         return "--"
     return formats.localize(value, use_l10n=False)
+
+
+@register.filter
+def get_item(dictionary, key):
+    return dictionary.get(key)
